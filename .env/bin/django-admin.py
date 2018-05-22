bb26aa72f1d9d8f27174380737e0df6057d5cbdd
RhiaKe <rhiakaulitzke@gmail.com>
2018-05-22 11:55:44 -0600

    first commit, done with part1

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/rhiannonkaulitzke/wdi-work/dayWork/catcollectr/catcollectr/.env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
